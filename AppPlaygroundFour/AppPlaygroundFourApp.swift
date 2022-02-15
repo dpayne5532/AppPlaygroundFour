ce9587f476969ec2dd5a66834eb5ffcd010bfa1c
Dan Payne <dpayne5532@gmail.com>
2022-02-14 18:30:23 -0700

    Inital Commit

---
+++ AppPlaygroundFour/AppPlaygroundFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPlaygroundFourApp.swift
+//  AppPlaygroundFour
+//
+//  Created by Dan Payne on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AppPlaygroundFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
